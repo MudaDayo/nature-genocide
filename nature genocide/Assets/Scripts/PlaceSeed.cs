15441f08f2c8689d37ceed5b794e3d97d9ec21e3
CaTTco2 <nnicolashh@gmail.com>
2024-09-27 18:54:21 +0200

    Changed seed name in placeSeed class

--- nature genocide/Assets/Scripts/PlaceSeed.cs
+++ nature genocide/Assets/Scripts/PlaceSeed.cs
@@ -4,7 +4,7 @@ public class PlaceSeed : MonoBehaviour
 {
     [SerializeField] Camera _playerCamera;
 
-    public GameObject holdedSeed;
+    public GameObject seedPot;
     public GameObject previewHoldedSeed;
     private GameObject _previewGameObject;
 
@@ -19,13 +19,13 @@ public class PlaceSeed : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.E) && holdedSeed != null)
+        if (Input.GetKeyUp(KeyCode.E) && seedPot != null)
         {
             Physics.Raycast(this.transform.position, Vector3.down, out RaycastHit hitInfo);
-            Instantiate(holdedSeed, new Vector3(_playerCamera.transform.position.x, 
+            Instantiate(seedPot, new Vector3(_playerCamera.transform.position.x, 
                 hitInfo.collider.transform.position.y + 0.2F, _playerCamera.transform.position.y), Quaternion.identity);
 
-            holdedSeed = null;
+            seedPot = null;
         }
     }
 }
