0541e65fd337bcfb771a8f7f647676d92ff0b12a
CaTTco2 <nnicolashh@gmail.com>
2024-09-28 18:26:49 +0200

    Collision Does damage

--- nature genocide/Assets/Scripts/MeteoriteDamageCollider.cs
+++ nature genocide/Assets/Scripts/MeteoriteDamageCollider.cs
@@ -5,10 +5,19 @@ public class MeteoriteDamageCollider : MonoBehaviour
     [SerializeField] private float _lifeTime;
     private float _timer;
 
+    private GameObject UIManager;
+    [SerializeField] private GameObject hpManager;
+
+    [SerializeField] private float _knockupForce;
+
+    private bool _hasCheckedTrigger = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        hpManager = GameObject.Find("HeartManager");
+
+        UIManager = GameObject.FindGameObjectWithTag("UIManager");
     }
 
     // Update is called once per frame
@@ -18,6 +27,28 @@ public class MeteoriteDamageCollider : MonoBehaviour
         if (_timer >= _lifeTime)
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (_hasCheckedTrigger == false)
+        {
+            _hasCheckedTrigger = true;
+
+            if (other.gameObject.tag == "Player")
+            {
+                UIManager.GetComponent<redPanel>().Enable();
+
+                hpManager.GetComponent<PlayerHP>().LoseHP();
+                other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0f, _knockupForce, 0f));
+            }
+
+            if (other.gameObject.tag == "Enemy")
+            {
+                other.TryGetComponent<Enemy>(out Enemy enemyScript);
+                enemyScript.Die();
+            }
         }
     }
 }
