5fe1f67c2d94aa3f0ccdff7b3b65499005fb282e
CaTTco2 <nnicolashh@gmail.com>
2024-09-28 20:14:50 +0200

    Meteorite explosions

--- nature genocide/Assets/Scripts/MeteoriteHit.cs
+++ nature genocide/Assets/Scripts/MeteoriteHit.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class MeteoriteHit : MonoBehaviour
@@ -8,6 +10,8 @@ public class MeteoriteHit : MonoBehaviour
     private GameObject _parent;
     private Meteorite _parentScript;
 
+    [SerializeField] private int _numberOfExplosions = 5;
+    [SerializeField] private GameObject _explosion;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -25,14 +29,35 @@ public class MeteoriteHit : MonoBehaviour
     {
         if (other.gameObject.tag == "Floor")
         {
-            Explode();
             Instantiate(_seed, new Vector3(_parentScript._target.x, -3.75f, _parentScript._target.z), Quaternion.identity);
             Instantiate(_damageCollision, new Vector3(_parentScript._target.x, -3.75f, _parentScript._target.z), Quaternion.identity);
+
+            StartCoroutine(SpawnExplosions());
+            StartCoroutine(Explode());
         }
     }
 
-    private void Explode()
+    private IEnumerator SpawnExplosions()
     {
+        for (int i = 0; i < _numberOfExplosions; i++)
+        {
+            SpawnExplosion();
+            yield return null;
+        }
+    }
+
+    private void SpawnExplosion()
+    {
+        Debug.Log("Spawned Explosion");
+        Vector3 randomSpawnPos = UnityEngine.Random.insideUnitSphere * 5;
+
+        Instantiate(_explosion, this.transform.position + randomSpawnPos + new Vector3(0f, 10.5f, 0f), Quaternion.identity);
+    }
+
+    private IEnumerator Explode()
+    {
+        yield return new WaitForSeconds(0.05f);
+
         Debug.Log("boooom");
         Destroy(this.transform.parent.gameObject);
     }
