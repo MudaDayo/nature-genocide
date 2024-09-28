81384e12b0a6d98ccc02d441954c3430ee4a499d
Dan Chicu <dan@chicu.ro>
2024-09-28 18:04:08 +0200

    E

--- nature genocide/Assets/Scripts/PlaceSeed.cs
+++ nature genocide/Assets/Scripts/PlaceSeed.cs
@@ -5,7 +5,7 @@ public class PlaceSeed : MonoBehaviour
     [SerializeField] GameObject _rayCastStartPos;
     [SerializeField] GameObject _seedSpawnPos;
 
-    [SerializeField] GameObject handHoldEmpty, handHoldFull, handGrab, grabPrefab;
+    [SerializeField] GameObject handHoldEmpty, handHoldFull, handGrab, grabPrefab, ButtonPromptE;
 
     public GameObject seedPot;
     public GameObject previewHoldedSeed;
@@ -24,6 +24,8 @@ public class PlaceSeed : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E) && seedPot != null)
         {
+            ButtonPromptE.SetActive(false);
+
             Physics.Raycast(_rayCastStartPos.transform.position, Vector3.down, out RaycastHit hitInfo);
             Instantiate(seedPot, new Vector3(_seedSpawnPos.transform.position.x, 
                 hitInfo.point.y, _seedSpawnPos.transform.position.z), Quaternion.identity);
