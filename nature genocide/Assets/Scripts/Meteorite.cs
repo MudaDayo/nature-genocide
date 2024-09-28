9ed285f2e2c275a07a03c5ec3c592ca78960e5fc
CaTTco2 <nnicolashh@gmail.com>
2024-09-28 13:04:15 +0200

    Meteorite progress

---
+++ nature genocide/Assets/Scripts/Meteorite.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Meteorite : MonoBehaviour
+{
+    private GameObject _player;
+    private Vector3 _target;
+    private Vector3 _direction;
+
+    [SerializeField] private float _speed;
+
+    [SerializeField] private GameObject _tempVisualiser;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("PlayerAttackThing");
+        Debug.Log(_player.name);
+
+        Physics.Raycast(_player.transform.position, Vector3.down, out RaycastHit hitInfo);
+        _target = new Vector3(_player.transform.position.x, hitInfo.point.y, _player.transform.position.z);
+
+        Debug.Log(_target);
+        Instantiate(_tempVisualiser, _target, Quaternion.identity);
+
+
+        transform.LookAt(_target);
+        _direction = (_target - this.transform.position);
+        _direction.Normalize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += _direction * _speed * Time.deltaTime;
+    }
+}
